7e17d1e7959b9ec2df3fdbc064bdab6c1d548266
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-11-12 15:24:19 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  
   devise_for :user, skip: [:passwords], controllers: {
     registrations: "user/registrations",
     sessions: 'user/sessions'
   }
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  
+  scope module: :user do
+    root :to => "posts#index"
+    resources :posts
+    resources :users, only: [:show,:edit,:update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
