007ba82ff7622f5284e3401363dfdfbe15afa901
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-11-15 07:26:34 +0000

    [add]tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_062650) do
+ActiveRecord::Schema.define(version: 2022_11_14_140723) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,22 @@ ActiveRecord::Schema.define(version: 2022_11_11_062650) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tagmaps", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_tagmaps_on_post_id"
+    t.index ["tag_id"], name: "index_tagmaps_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_name"], name: "index_tags_on_tag_name", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,4 +90,6 @@ ActiveRecord::Schema.define(version: 2022_11_11_062650) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "tagmaps", "posts"
+  add_foreign_key "tagmaps", "tags"
 end
