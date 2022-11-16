136a44329812ff3b0debecb5f09064393d2f5be9
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-11-16 06:53:15 +0000

    [fix]comment

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -17,7 +17,7 @@ class User::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @post_tags = @post.tags
+    @post_comment = PostComment.new
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   scope module: :user do
     root :to => "posts#index"
     get "search" => "searches#search"
-    resources :posts
+    resources :posts do
+      resources :post_comments, only: [:create,:destroy]
+    end
     resources :users, only: [:show,:edit,:update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
