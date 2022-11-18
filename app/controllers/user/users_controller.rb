8489e17fab2c17e711a78b896433bf964bcbb387
FujisawaAkifumi <112544215+fujisawa1234@users.noreply.github.com>
2022-11-18 08:57:32 +0000

    [add]guest

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,4 +1,7 @@
 class User::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -6,6 +9,11 @@ class User::UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to posts_path
+    end
   end
 
   def update
@@ -16,7 +24,7 @@ class User::UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   def favorites
     @user = User.find(params[:id])
     favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
@@ -28,5 +36,12 @@ class User::UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name,:profile_image)
+    end
+
+    def ensure_guest_user
+      @user = User.find(params[:id])
+      if @user.name == "guestuser"
+        redirect_to posts_path, notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+      end
     end
 end
